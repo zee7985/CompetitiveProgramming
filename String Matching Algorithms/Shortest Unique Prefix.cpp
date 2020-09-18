00dd300d6d66b798a40251976b72363b0cbaf514
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-09-19 00:32:12 +0530

    trie

--- String Matching Algorithms/Shortest Unique Prefix.cpp
+++ String Matching Algorithms/Shortest Unique Prefix.cpp
@@ -1,30 +1,30 @@
-Find shortest unique prefix for every word 
-Given an array of words, find all shortest unique prefixes to represent each word in the given array.
- Assume that no word is prefix of another.
+// Find shortest unique prefix for every word 
+// Given an array of words, find all shortest unique prefixes to represent each word in the given array.
+//  Assume that no word is prefix of another.
 
-Examples:
+// Examples:
 
-Input: arr[] = {"zebra", "dog", "duck", "dove"}
-Output: dog, dov, du, z
-Explanation: dog => dog
-             dove = dov 
-             duck = du
-             zebra   => z
+// Input: arr[] = {"zebra", "dog", "duck", "dove"}
+// Output: dog, dov, du, z
+// Explanation: dog => dog
+//              dove = dov 
+//              duck = du
+//              zebra   => z
 
-Input: arr[] =  {"geeksgeeks", "geeksquiz", "geeksforgeeks"};
-Output: geeksf, geeksg, geeksq}
+// Input: arr[] =  {"geeksgeeks", "geeksquiz", "geeksforgeeks"};
+// Output: geeksf, geeksg, geeksq}
 
 #include<bits/stdc++.h>
 using namespace std;
 
-1) Construct a Trie of all words. Also maintain frequency of every node
- (Here frequency is number of times node is visited during insertion).
-  Time complexity of this step is O(N) where N is total number of characters in all words.
+// 1) Construct a Trie of all words. Also maintain frequency of every node
+//  (Here frequency is number of times node is visited during insertion).
+//   Time complexity of this step is O(N) where N is total number of characters in all words.
 
-2) Now, for every word, we find the character nearest to the root with frequency as 1.
- The prefix of the word is path from root to this character. To do this, we can traverse
-  Trie starting from root. For every node being traversed, we check its frequency.
-   If frequency is one, we print all characters from root to this node and don’t traverse down this node.
+// 2) Now, for every word, we find the character nearest to the root with frequency as 1.
+//  The prefix of the word is path from root to this character. To do this, we can traverse
+//   Trie starting from root. For every node being traversed, we check its frequency.
+//    If frequency is one, we print all characters from root to this node and don’t traverse down this node.
 
 class trie{
     public:
